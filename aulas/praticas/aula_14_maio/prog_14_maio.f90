91fb077f3fefbfe7f2b396356daf1d9b0ae442f3
SaganGromov <matheus.andrade5488@gmail.com>
2025-05-18 04:52:50 -0300

    atualizacao em 18/05/2025 04:52:50

--- aulas/praticas/aula_14_maio/prog_14_maio.f90
+++ aulas/praticas/aula_14_maio/prog_14_maio.f90
@@ -6,15 +6,15 @@ program exercicio
   double precision, dimension (3,3) :: a
   integer :: n
 
-  a(1,1) =-1.d0
-  a(1,2) =2.d0
-  a(1,3) =2.d0
-  a(2,1) =-1.d0
-  a(2,2) =-4.d0
-  a(2,3) =-2.d0
-  a(3,1) =-3.d0
-  a(3,2) =9.d0
-  a(3,3) =7.d0
+  a(1,1) = -14.d0/11.d0
+  a(1,2) = -8.d0/11.d0
+  a(1,3) = -42.d0/11.d0
+  a(2,1) = 25.d0/11.d0
+  a(2,2) =-47.d0/11.d0
+  a(2,3) =108.d0/11.d0
+  a(3,1) =13.d0/11.d0
+  a(3,2) =-13.d0/11.d0
+  a(3,3) =50.d0/11.d0
   
   x(1) = 1.d0
   x(2) = 1.d0
@@ -23,9 +23,12 @@ program exercicio
   do n = 1, 100
      xold = x
      x = mult(A,x,3)
-     !write(*,*) x
-     !read(*,*)
-     write(*,*) x(1)/xold(1), x(2)/xold(2), x(3)/xold(3), x/((x(1)/xold(1))**n)
+     write(*,*) "Iteration:", n
+     write(*,*) "Current vector x: ", x
+     write(*,*) "Previous vector xold: ", xold
+     write(*,*) "Ratios x(i)/xold(i): ", x(1)/xold(1), x(2)/xold(2), x(3)/xold(3)
+     write(*,*) "Normalized vector: ", x/((x(1)/xold(1))**n)
+     write(*,*) "----------------------------------------"
   end do
 
 contains
